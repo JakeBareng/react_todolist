f56d42f72cbf5476fbb3308eb40b1bcd7aeec0d8
JakeBareng <barengjake@gmail.com>
2023-02-04 20:47:57 -0800

    - moved functions into a utility.js file - added EditToDo.js - added on hover, conditional render

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 const ToDo = ({todo, toggle}) => {
+    const [showEdit, setIsShown] = useState(false);
+    const [showEditForm, setEditForm] = useState(true);
+
     const handleEvent = () => {
         toggle(todo.id)
     }
-    return (
-        <div className={todo.done?"done clickable task":"not-done clickable task"} onClick={handleEvent}>
-            {todo.todo}
-        </div>
-    )
+
+    const conditionalRender = () => {
+        if (showEdit) {
+            return (
+                <div 
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
+                >
+                    <div
+                    className={todo.done?"done clickable task":"not-done clickable task"} 
+                    onClick={handleEvent}
+                    >
+                        <p>{todo.todo}</p>
+                    </div>
+                    <button>edit</button>
+                </div>
+            )
+        }
+        return (
+            <div 
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+            >
+                <div
+                className={todo.done?"done clickable task":"not-done clickable task"} 
+                >
+                    <p>{todo.todo}</p>
+                </div>
+            </div>
+        )
+
+    }
+
+    return conditionalRender();
 }
 
 export default ToDo;
